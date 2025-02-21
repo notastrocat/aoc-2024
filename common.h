6d67494d19bdb1c22c397b6b5680827a28f64064
Archit Sharma <archit.sharma@aptiv.com>
2025-02-21 19:30:37 +0000

    adding a common header which could prove to be useful later

---
+++ common.h
@@ -0,0 +1,12 @@
+// color codes to be able to log various things - basic inspired from various tools
+enum colorCodes {
+  FG_BLACK = 30,
+  FG_RED = 31,
+  FG_GREEN = 32,
+  FG_YELLOW = 33,
+  FG_BLUE = 34,
+  FG_MAGENTA = 35,
+  FG_CYAN = 36,
+  FG_WHITE = 37,
+  FG_DEF = 39
+};
